cd3ba5bfcb7b3f09c5fb2778b45671f789f9ee45
SUAT SATILMIŞ <suat.satilmis@ym.com.tr>
2025-02-02 15:22:40 +0300

    Shared

---
+++ src/Shared/Core.Application/Pipelines/Authorization/AuthorizationBehavior.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using NArchitecture.Core.CrossCuttingConcerns.Exception.Types;
+using NArchitecture.Core.Security.Constants;
+using NArchitecture.Core.Security.Extensions;
+
+namespace NArchitecture.Core.Application.Pipelines.Authorization;
+
+public class AuthorizationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>, ISecuredRequest
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public AuthorizationBehavior(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken
+    )
+    {
+        if (!_httpContextAccessor.HttpContext.User.Claims.Any())
+            throw new AuthorizationException("You are not authenticated.");
+
+        if (request.Roles.Any())
+        {
+            ICollection<string>? userRoleClaims = _httpContextAccessor.HttpContext.User.GetRoleClaims() ?? [];
+            bool isMatchedAUserRoleClaimWithRequestRoles = userRoleClaims.Any(userRoleClaim =>
+                userRoleClaim == GeneralOperationClaims.Admin || request.Roles.Contains(userRoleClaim)
+            );
+            if (!isMatchedAUserRoleClaimWithRequestRoles)
+                throw new AuthorizationException("You are not authorized.");
+        }
+
+        TResponse response = await next();
+        return response;
+    }
+}
